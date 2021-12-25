3e32bd744f3865b2088e9d86e63e3cb1a3bed8c3
keyy123 <parktwins1@gmail.com>
2021-12-24 23:33:45 -0500

    I am practicing pushing mongoose models to my database admin tool of choice. I was playing around with moving models to mongoDB database. It's pretty fun and easier than the pure mongoDB CRUD operations which aren't too difficult to pick up but take up a lot of screen space.

--- task-manager/db/mongoose.js
+++ task-manager/db/mongoose.js
@@ -1,3 +1,39 @@
-const {mongoose} = require('mongoose')
+const { connect, model, Schema} = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{useNewUrlParser:true})
+connect('mongodb://127.0.0.1:27017/task-manager-api',{})
+
+// const UserSchema = new Schema({
+//     name: {type:String, default: "Guest"},
+//     age: {type: Number, default: 1}
+// })
+
+// const User = new model('User', UserSchema)
+
+// const me = new User({
+//     name:"Khayyon", 
+//     age: 24
+// })
+
+// me.save().then(()=>{
+// console.log(me)
+// }).catch((error)=>{
+// console.error({error: error});
+// })
+
+const TaskSchema = new Schema({
+    details: {type: String},
+    done: {type: Boolean}
+})
+
+const Tasks = new model('Tasks', TaskSchema)
+
+const myTasks = new Tasks({
+    details: "Prepare Large Trash For Bulk Day",
+    done: false
+})
+
+myTasks.save().then(()=>{
+    console.log(myTasks)
+}).catch((error)=>{
+    console.error({error:error});
+})
