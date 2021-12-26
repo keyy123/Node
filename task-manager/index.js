3ff14bce2e6ba794a2c38cea5e7349539c54a80c
keyy123 <parktwins1@gmail.com>
2021-12-26 18:31:14 -0500

    I made some get and post routes via mongoose. I was questioning when to use a parameter for the promises. I've decided that if the document is saved I don't need a params b/c I can return the new document. I find it useful for the get routes to do the opposite. Otherwise, It was a chill sunday

--- task-manager/index.js
+++ task-manager/index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 require('./db/mongoose') //import mongoose connection so it runs when express server runs
 const User = require('./model/user') //imports user model for use in express server
-
+const Task = require('./model/task')
 const port = process.env.PORT || 3001
+const {ObjectId} = require('mongoose')
 
 app.use(express.json()) //allows us to receive req.body as json
 
@@ -11,18 +12,57 @@ app.get("/", (req, res)=>{
     res.send("Welcome to Express")
 })
 
-// app.get("/users/:id", (req,res)=>{
-//         res.send(req.query)
-// })
+app.get("/users/:id", (req,res)=>{
+        res.send(req.query)
+})
+
+app.get("/users", (req, res)=>{
+   User.find({}).then((users)=>{
+        res.send(users)
+    }).catch((error)=>{
+        res.status(404).send(error)
+    })
+    
+    //res.status(200).send(users)
+})
+
+app.get("/users/:id", (req, res)=>{
+   let id = req.params.id
+    // User.findOne({_id:id}).then((user)=>{
+    //     res.status(200).send(user)
+    // }).catch((error)=>{
+    //     res.status(404).send(error)
+    // })
+
+    //Both works but findbyid works with a little less thought but its preference I guess
+    User.findById(id).then((user)=>{
+        res.status(200).send(user)
+    }).catch((error)=>{
+        res.status(404).send(error)
+    })
+})
 
 app.post("/users", (req,res)=>{
     const newUser = new User(req.body)
-    newUser.save().then(()=>{
-        res.status(201).send(newUser)
+    newUser.save().then((user)=>{
+        res.status(201).send(user)
     }).catch((error)=>{
         res.status(400).send(error)
     })
 })
+
+//Using a param for .then or w/o param works 
+
+app.post("/tasks", (req, res)=>{
+const newTask = new Task(req.body)
+newTask.save().then(()=>{
+res.status(201).send(newTask)
+}
+).catch((error)=>{
+res.status(400).send(error)
+})
+})
+
 
 app.listen(port, ()=>{
    return console.log(`listening to connection at http://localhost:${port}`)
