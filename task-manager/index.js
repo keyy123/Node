647563d4531f6e02632a4660f8f04b6f16f64924
keyy123 <parktwins1@gmail.com>
2021-12-28 15:42:01 -0500

    I reviewed bcrypt. I learned more about its methods and how to hash and compare password strings to hash. I learned more about salt rounds, why every serious app should securely store passwords and the obvious but underrated dangers of securely storing the users passwords.

--- task-manager/index.js
+++ task-manager/index.js
@@ -9,12 +9,34 @@ app.use(express.json())
 app.use('/',routes)
 
 
-
-//Using a param for .then or w/o param works 
-
-//Task Routes
-
-
 app.listen(port, ()=>{
    return console.log(`listening to connection at http://localhost:${port}`)
 })
+
+const bcrypt = require('bcryptjs')
+
+const work = async() => {
+const pass = '123456789'
+const hashPass = await bcrypt.hash(pass, 8,async(err, code)=>{
+   if(err){
+      return console.log('password not hashed')
+   }
+   console.log(pass)
+   console.log(code)
+   const Truth = await bcrypt.compare(pass, code, (error, value)=>{
+      if(error){
+         return console.log('could not compare password and hash')
+      }
+      console.log(`It is ${value}, The hash and password are the same`)
+   })
+},((num)=>{
+   console.log(num)
+}))
+// const match = await bcrypt.compare(pass,code).then((value)=>{
+//    console.log(value)
+// }).catch((e)=>{
+//    console.log(e)
+// })
+}
+
+work()
