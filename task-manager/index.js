455419ce70ae3d100d8e498b1561b9ed282b6bd0
keyy123 <parktwins1@gmail.com>
2021-12-28 00:51:56 -0500

    I refactor my promise-chains into async/await which was very helpful to us

--- task-manager/index.js
+++ task-manager/index.js
@@ -1,104 +1,139 @@
 const express = require('express')
 const app = express()
-require('./db/mongoose') //import mongoose connection so it runs when express server runs
-const User = require('./model/user') //imports user model for use in express server
+require('./db/mongoose') 
+const User = require('./model/user') 
 const Task = require('./model/task')
 const port = process.env.PORT || 3001
 
-app.use(express.json()) //allows us to receive req.body as json
+app.use(express.json()) 
 
 app.get("/", (req, res)=>{
     res.send("Welcome to Express")
 })
 
-app.get("/users/:id", (req,res)=>{
-        res.send(req.query)
+
+app.get("/users", async (req, res)=>{
+try{
+    const users = await User.find(req.body);
+if(!users){
+    res.status(404).send();
+}
+res.status(200).send(users);
+}catch(e){
+    res.status(404).send(e);
+}
 })
 
-app.get("/users", (req, res)=>{
-   User.find(req.body).then((users)=>{
-       if(!user){
-           res.status(404).send()
-       }
-        res.send(users)
-    }).catch((error)=>{
-        res.status(404).send(error)
-    })
-    
-    //res.status(200).send(users)
-})
 
-app.get("/users/:id", (req, res)=>{
-   let id = req.params.id
-    // User.findOne({_id:id}).then((user)=>{
-    //     res.status(200).send(user)
-    // }).catch((error)=>{
-    //     res.status(404).send(error)
-    // })
-
-    //Both works but findbyid works with a little less thought but its preference I guess
-    User.findById(id).then((user)=>{
+app.get("/users/:id", async(req,res)=> {
+    let id = req.params.id;
+    try{
+        const user = await User.findById(id)
+        console.log(user)
         if(!user){
             res.status(404).send()
         }
-        res.status(200).send(user)
-    }).catch((error)=>{
-        res.status(404).send(error)
-    })
+        res.send(user)
+    }catch(e){
+        res.status(500).send()
+    }
 })
 
-app.post("/users", (req,res)=>{
+
+
+
+
+app.post("/users", async (req,res)=>{
     const newUser = new User(req.body)
-    newUser.save().then((user)=>{
-        if(!user){
-            res.status(400).send()
-        }
-        res.status(201).send(user)
-    }).catch((error)=>{
-        res.status(400).send(error)
-    })
+    // newUser.save().then((user)=>{
+    //     if(!user){
+    //         res.status(400).send()
+    //     }
+    //     res.status(201).send(user)
+    // }).catch((error)=>{
+    //     res.status(400).send(error)
+    // })
+try{
+if(!newUser){
+    res.status(400).send()
+}
+await newUser.save()
+res.status(201).send(newUser)
+}catch(e){
+    res.status(400).send(e)
+}
 })
 
 //Using a param for .then or w/o param works 
 
-
-app.post("/tasks", (req, res)=>{
+//Task Routes
+app.post("/tasks", async (req, res)=>{
+// const newTask = new Task(req.body)
+// newTask.save().then(()=>{
+//     if(!newTask){
+//         res.status(400).send()
+//     }
+// res.status(201).send(newTask)
+// }
+// ).catch((error)=>{
+// res.status(400).send(error)
+// })
 const newTask = new Task(req.body)
-newTask.save().then(()=>{
+try{
     if(!newTask){
         res.status(400).send()
     }
-res.status(201).send(newTask)
+await newTask.save()
+res.send(newTask)
+}catch(e){
+    res.status(500).send()
 }
-).catch((error)=>{
-res.status(400).send(error)
-})
 })
 
 
 //Task GET endpoints
-app.get("/tasks", (req,res)=>{
-    Task.find({}).then((tasks)=>{
-        if(!tasks){
+app.get("/tasks", async (req,res)=>{
+    // Task.find({}).then((tasks)=>{
+    //     if(!tasks){
+    //         res.status(404).send()
+    //     }
+    //     res.status(200).send(tasks)
+    // }).catch((error)=>{
+    //     res.status(404).send(error)
+    // })
+    try{
+        const allTasks = await Task.find({})
+        const taskCount = await Task.countDocuments({})
+        console.log(taskCount)
+        if(!allTasks){
             res.status(404).send()
         }
-        res.status(200).send(tasks)
-    }).catch((error)=>{
-        res.status(404).send(error)
-    })
+        res.status(200).send(allTasks)
+    }catch(e){
+        res.status(500).send()
+    }
 })
 
-app.get("/tasks/:id", (req, res)=>{
-    console.log(req.params)
+app.get("/tasks/:id", async (req, res)=>{
     let id = req.params.id
-    Task.findById(id).then((task)=>{
-        if(!task){
+    // Task.findById(id).then((task)=>{
+    //     if(!task){
+    //         res.status(404).send()
+    //     }
+    //     res.status(200).send(task)
+    // }).catch((error)=>{
+    //     res.status(404).send(error)
+    // })
+
+    try{
+        const oneTask = await Task.findById(id)
+        if(!oneTask){
             res.status(404).send()
         }
-        res.status(200).send(task)
-    }).catch((error)=>{
-        res.status(404).send(error)
-    })
+        res.send(oneTask)
+    }catch(e){
+        res.status(500).send()
+    }
 })
 
 
--- task-manager/playground/test.js
+++ task-manager/playground/test.js
@@ -1,28 +1,28 @@
-require('../db/mongoose.js')
-const User = require('../model/user')
-// User.findOneAndUpdate({name:"Guest"},{name:"BJ"}).then((user)=>{
-//     if(!user){
-//         res.status(404).send()
+// require('../db/mongoose.js')
+// const User = require('../model/user')
+// // User.findOneAndUpdate({name:"Guest"},{name:"BJ"}).then((user)=>{
+// //     if(!user){
+// //         res.status(404).send()
+// //     }
+// //     console.log(user)
+// //     return User.countDocuments({name:"BJ"}).then((docs)=>{
+// //         console.log(docs)
+// //     })
+// // }).catch((e)=>{
+// //     console.log(e)
+// // })
+
+// const updateUserAndCount = async(query,name) =>{
+//     const user = await User.findOneAndUpdate({query},{name})
+//     const count = await User.countDocuments({name})
+//     return count
+// }
+
+// updateUserAndCount({name:"BJ"},"Bryant Jr").then((count)=>{
+//     if(!count){
+//         console.log('Update was not successful')
 //     }
-//     console.log(user)
-//     return User.countDocuments({name:"BJ"}).then((docs)=>{
-//         console.log(docs)
-//     })
+//     console.log(count)
 // }).catch((e)=>{
 //     console.log(e)
 // })
-
-const updateUserAndCount = async(query,name) =>{
-    const user = await User.findOneAndUpdate({query},{name})
-    const count = await User.countDocuments({name})
-    return count
-}
-
-updateUserAndCount({name:"BJ"},"Bryant Jr").then((count)=>{
-    if(!count){
-        console.log('Update was not successful')
-    }
-    console.log(count)
-}).catch((e)=>{
-    console.log(e)
-})
